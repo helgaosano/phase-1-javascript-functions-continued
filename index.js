8e244adf0a25be02f9713dd8de6ed67adff25a71
tania <taniaosano15@gmail.com>
2022-08-21 21:53:07 +0300

    Completed functions continued lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun("roller-skate");
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+mondayWork("go to the office");
+
+function wrapAdjective( comment = '*'){
+   
+return function(name2 = "special"){
+    return `You are ${comment}${name2}${comment}!`
+    }  
+       
+}
